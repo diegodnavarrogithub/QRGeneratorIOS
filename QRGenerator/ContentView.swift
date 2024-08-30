e408ed1f34940c25f705a888a3937b2e6f5469fa
Diego <100453189+diegodnavarrogithub@users.noreply.github.com>
2024-08-30 09:10:38 -0600

    Initial Commit

---
+++ QRGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRGenerator
+//
+//  Created by Diego Dominguez on 30/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRGenerator/QRGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRGeneratorApp.swift
+//  QRGenerator
+//
+//  Created by Diego Dominguez on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct QRGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
